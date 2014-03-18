ff10eaba0f7f3b0ecc7df0a1c8dabdb4d971f972
Andy Cox <acox@weather.com>
2014-03-18 15:58:44 -0400

    Engine initializer is the preferred way

--- lib/tipsy/rails/engine.rb
+++ lib/tipsy/rails/engine.rb
@@ -1,6 +1,9 @@
 module Tipsy
   module Rails
     class Engine < ::Rails::Engine
+      initializer "tipsy-rails.assets.precompile" do |app|
+        app.config.assets.precompile += %w(tipsy.gif)
+      end
     end
   end
 end
